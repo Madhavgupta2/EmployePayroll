86b99a3c4110ee9bf91c1a36524fb59dbf3569dc
Madhavgupta2 <madhavgupta222001@gmail.com>
2025-03-20 13:43:52 +0530

    uc7

---
+++ EmployeePayroll-UC7/EmployeePayroll-UC7/src/main/java/com/example/EmployeePayRoll/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.example.EmployeePayRoll.controller;
+
+import com.example.EmployeePayRoll.model.Employee;
+import com.example.EmployeePayRoll.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = service.getEmployeeById(id);
+        return employee != null ? ResponseEntity.ok(employee) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/add")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        boolean deleted = service.deleteEmployee(id);
+        return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
+}
